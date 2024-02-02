7d8d886ff0dfa648f661117c1d808803d41d9447
juanf1328 <jforni@SOFTWARE-FACTORY.ENCODESA.INTERNAL>
2024-02-02 16:49:16 -0300

    new

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,14 +1,45 @@
 // MainPage.js
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import './MainPage.css';
 
 const MainPage = () => {
   // Contenido de la página principal después de iniciar sesión
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Main');
+
+    
+    navigate('/finish');
+  };
 
   return (
-    <div>
-      <h1>Main Page</h1>
-      {/* Otros componentes y contenido */}
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div className='conteiner'>
+      <div>
+        <h1>Main Page</h1>
+        </div>
+        <div>
+          <button className='main1' onClick={handleSubmit} type="submit">Muro</button>
+        </div>
+        <div>
+          <button className='main1' type="submit">Mercado AI</button>
+        </div>
+        <div>
+          <button className='main1' type="submit">Cuentas</button>
+        </div>
+        <div>
+          <button className='main1' type="submit">Ayuda</button>
+        </div>
+        <div>
+          <button className='main1' type="submit">Salir</button>
+        </div>
+
+      </div>
+    </form>
   );
 };
 
